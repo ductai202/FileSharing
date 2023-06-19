3314e5539bd43fcbfe91bce291081967ebd7bcea
ductai202 <ductainguyentran@gmail.com>
2023-06-19 21:57:21 +0700

    add signalR

---
+++ FileSharing404/FileSharing404/Program.cs
@@ -0,0 +1,41 @@
+using FileSharing404;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSignalR();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        builder =>
+        {
+            builder.WithOrigins("http://127.0.0.1:5500/")
+                .AllowAnyHeader()
+                .WithMethods("GET", "POST")
+                .AllowCredentials();
+        });
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+// UseCors must be called before MapHub.
+app.UseCors();
+app.UseRouting();
+app.MapHub<WebRtcHub>("/chatHub");
+
+app.Run();
